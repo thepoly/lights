8976dcff8aecaf98b527910532b934076bac00ab
jlyon1 <joeylyon1@gmail.com>
2017-03-06 13:38:09 -0500

    continuing gui updates

--- server/test.php
+++ server/test.php
@@ -1,16 +1,3 @@
 <?php
-error_reporting(0);
-if($_POST['colors'] != ''){
-	$colors = explode(',',$_POST['colors']);//expand color variables to an array
-	$textfile = "LED.txt"; // Declares the name and location of the .txt file
-
-	$fileLocation = "$textfile";
-	$fh = fopen($fileLocation, 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
-	fwrite($fh, $colors[0]."\n");
-	fwrite($fh, $colors[1]."\n");
-	fwrite($fh, $colors[2]."\n");
-	fclose($fh);
-
-	header("HTTP/1.0 200 OK");
-}
+echo $_POST['word']
 ?>
