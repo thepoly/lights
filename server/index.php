4594d623455f7027b6b13a034325b3b840fc6ca6
jlyon1 <joeylyon1@gmail.com>
2017-03-06 14:36:13 -0500

    php now writes values to file

--- server/index.php
+++ server/index.php
@@ -1,192 +1,95 @@
-<html>
+<?php
+error_reporting(0);
+if($_POST['colors'] != ''){
+	$colors = explode(',',$_POST['colors']);//expand color variables to an array
+	$textfile = "LED.txt"; // Declares the name and location of the .txt file
 
-<head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script>
-var quotes = [
-  "Poly Lights"
+	$fileLocation = "$textfile";
+	$fh = fopen($fileLocation, 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
+	fwrite($fh, $colors[0]."\n");
+	fwrite($fh, $colors[1]."\n");
+	fwrite($fh, $colors[2]."\n");
+	fclose($fh);
 
-];
-var q = quotes.length - 1;
-var whichquote=Math.round(Math.random()*(q));
-var nah = false;;
-$('div').on('click', function(){
-    alert($(this).attr("id"));
-});
-
-function wurd(val,name){
-  if(!nah){
-    $(val).html($(val).children());
-    $(name).fadeIn(500);
-  }
-};
-
-function changeColor(name,color,letter,dnc){
-  nah = true;
-  $(name).css('background-color',color);
-  $(name).append(letter);
-  $(dnc).hide();
-  setTimeout(function(){
-    nah = false;
-}, 1000);
+	header("HTTP/1.0 200 OK");
 }
+?>
+<html>
+<head>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
-$(document).ready(function(){
-	$("body").hide();
-	$("#demo").html(quotes[whichquote]);
-	$("body").fadeIn(1000);
-  $(".do-not-clear").hide();
-  $(".do-not-clear-2").hide();
-  $(".do-not-clear-3").hide();
-  $(".do-not-clear-4").hide();
-  $.ajax({
-   type: "POST",
-     url: 'index.html',
-     data: {'word=':"aWord"},
-     success: function(data) {
-       alert(data);
-
-     }
-   });
-});
-
-
-</script>
-	<title>Poly Lights</title>
-  <link rel="stylesheet" type="text/css" href="hover.css">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans">
+	<script src='https://poly.rpi.edu/lights/spectrum.js'></script>
+	<link rel='stylesheet' href='https://poly.rpi.edu/lights/spectrum.css' />
 	<style>
-  .title{
-		margin: 0 auto;
-		width:50%;
+		body{
+		background-color: #e74c3c;
 
-		padding-top:0 ;
-		color: #2c3e50;
-  }
-  .color{
-    width: 20%;
-    height: 40%;
-    float: left;
-  }
-  .fourSelect{
-    width:50%;
-    height:50%;
-    position: relative;
-    float: left;
+		font-family: “Helvetica Neue”, Helvetica, Arial, sans-serif;
+		}
+		p{
+		font-size:60px;
 
-    background-color: black;
-  }
-	.lbox{
-		background-color: blue;
-		width: 800px;
-		height: 200px;
-		margin: 0 auto;
-		text-align: center;
-		font-size: 100px;
-		color:#2C3E50;
-	}
-	.boxOne {
-			margin-left: 0px;
-	    width: 200px;
-	    height: 200px;
-	    background: #4183D7;
-	    position: relative;
-	    top: 0px;
-	    left: 0px;
-	}
+		color: white;
+		}
 
-	.boxTwo {
-	    width: 200px;
-	    height: 200px;
-			background: #22A7F0;
-	    position: relative;
-	    left: 200px;
-	    top: -200px;
-	}
-	.boxThree {
-			width: 200px;
-			height: 200px;
-			background: #59ABE3;
-			position: relative;
-			left: 400px;
-			top: -400px;
-	}
-	.boxFour {
-			width: 200px;
-			height: 200px;
-			background: #81CFE0;
-			position: relative;
-			left: 600px;
-			top: -600px;
-	}
-	.left{
-		width:50%;
-		height: 100%;
-		background-color: #c0392b;
-		float:left;
-		border-radius: 50%;
-	}
-	.right{
-		width:50%;
-		height: 100%;
-		float: left;
-
-	}
-	body{
-    background-color: #ecf0f1;
-    background-image: url("canvas.png");
-    margin: 0;
-    padding: 0;
-	  font-family: Open Sans;
-		font-style: italic;
-		font-variant: normal;
-    color:#2C3E50;
-	}
-	p{
-		font-size:100px;
-    color:#2C3E50;
-	}
 	</style>
 </head>
 <body>
+	<script>
+	function getRGB(color) {
+	    var result;
+	    // Look for rgb(num,num,num)
+	    if (result = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(color)) return [parseInt(result[1]), parseInt(result[2]), parseInt(result[3])];
+	    // Look for rgb(num%,num%,num%)
+	    if (result = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(color)) return [parseFloat(result[1]) * 2.55, parseFloat(result[2]) * 2.55, parseFloat(result[3]) * 2.55];
+	    // Look for #a0b1c2
+	    if (result = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(color)) return [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
+	    // Look for #fff
+	    if (result = /#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(color)) return [parseInt(result[1] + result[1], 16), parseInt(result[2] + result[2], 16), parseInt(result[3] + result[3], 16)];
+}
+	</script>
 
-  <div class = "title">
-	   <center><p id = "demo"></p></center>
-  </div>
+	<center><p>Select a Color: <input type='text' id="minipicker"/></p></center>
 
-		<div class = "lbox">
-			<div onclick="wurd('.boxOne','.do-not-clear')" class = "boxOne">P
-        <div class = "do-not-clear">
-        <div class = "fourSelect" onclick="changeColor('.boxOne','#3498db','P','.do-not-clear') "style = "background-color: #3498db;"></div>
-        <div class = "fourSelect" onclick="changeColor('.boxOne','#e74c3c','P','.do-not-clear'); changeColor('.boxTwo','#e74c3c','')" style = "background-color: #e74c3c;"></div>
-        <div class = "fourSelect" onclick="changeColor('.boxOne','#2ecc71','P','.do-not-clear')" style = "background-color: #2ecc71;"></div>
-        <div class = "fourSelect" onclick="changeColor('.boxOne','#ecf0f1','P','.do-not-clear')" style = "background-color: #ecf0f1;"></div>
-      </div>
-			</div>
-				<div onclick="wurd('.boxTwo','.do-not-clear-2')" class = "boxTwo">O
-          <div class = "do-not-clear-2">
-          <div class = "fourSelect" onclick="changeColor('.boxTwo','#3498db','O','.do-not-clear-2') "style = "background-color: #3498db;"></div>
-          <div class = "fourSelect" onclick="changeColor('.boxTwo','#e74c3c','O','.do-not-clear-2');changeColor('.boxOne','#e74c3c','')" style = "background-color: #e74c3c;"></div>
-          <div class = "fourSelect" onclick="changeColor('.boxTwo','#2ecc71','O','.do-not-clear-2')" style = "background-color: #2ecc71;"></div>
-          <div class = "fourSelect" onclick="changeColor('.boxTwo','#ecf0f1','O','.do-not-clear-2')" style = "background-color: #ecf0f1;"></div>
-        </div>
-				</div>
-					<div onclick="wurd('.boxThree','.do-not-clear-3')" class = "boxThree">L
-            <div class = "do-not-clear-3">
-            <div class = "fourSelect" onclick="changeColor('.boxThree','#3498db','L','.do-not-clear-3') "style = "background-color: #3498db;"></div>
-            <div class = "fourSelect" onclick="changeColor('.boxThree','#e74c3c','L','.do-not-clear-3')" style = "background-color: #e74c3c;"></div>
-            <div class = "fourSelect" onclick="changeColor('.boxThree','#2ecc71','L','.do-not-clear-3')" style = "background-color: #2ecc71;"></div>
-            <div class = "fourSelect" onclick="changeColor('.boxThree','#ecf0f1','L','.do-not-clear-3')" style = "background-color: #ecf0f1;"></div>
-          </div>
-					</div>
-						<div onclick="wurd('.boxFour','.do-not-clear-4')" class = "boxFour">Y
-              <div class = "do-not-clear-4">
-              <div class = "fourSelect" onclick="changeColor('.boxFour','#3498db','Y','.do-not-clear-4') "style = "background-color: #3498db;"></div>
-              <div class = "fourSelect" onclick="changeColor('.boxFour','#e74c3c','Y','.do-not-clear-4')" style = "background-color: #e74c3c;"></div>
-              <div class = "fourSelect" onclick="changeColor('.boxFour','#2ecc71','Y','.do-not-clear-4')" style = "background-color: #2ecc71;"></div>
-              <div class = "fourSelect" onclick="changeColor('.boxFour','#ecf0f1','Y','.do-not-clear-4')" style = "background-color: #ecf0f1;"></div>
-            </div>
-						</div>
-		</div>
-</body>
+<script>
+
+$("#minipicker").spectrum({
+    color: "#FFFFFF",
+    showInput: true,
+    className: "full-spectrum",
+    showInitial: true,
+    showPaletteOnly: true,
+    showSelectionPalette: false,
+    maxPaletteSize: 10,
+    preferredFormat: "rgb",
+    localStorageKey: "spectrum.demo",
+    move: function (color) {
+
+    },
+    show: function () {
+
+    },
+    beforeShow: function () {
+
+    },
+    hide: function () {
+
+    },
+    change: function() {
+       $.ajax({
+				type: "POST",
+			  	url: 'index.php',
+			  	data: {'colors':getRGB($('#minipicker').val()).join(',')},
+			  	success: function(data) {
+            alert(data);
+			  	}
+			});
+    },
+    palette: [
+        ["rgb(255, 255, 255)", "rgb(255, 0, 0)", "rgb(0, 255, 0)",
+        "rgb(0, 0, 255)"]
+    ]
+});
+</script>
+	</body>
 </html>
--- server/test.php
+++ server/test.php
@@ -1,3 +1,13 @@
 <?php
-echo $_POST['word']
+$colors = explode(',',$_POST['word']);//expand color variables to an array
+$textfile = "LED.txt"; // Declares the name and location of the .txt file
+
+$fileLocation = "$textfile";
+$fh = fopen($fileLocation, 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
+fwrite($fh, $colors[0]."\n");
+fwrite($fh, $colors[1]."\n");
+fwrite($fh, $colors[2]."\n");
+fclose($fh);
+
+header("HTTP/1.0 200 OK");
 ?>
