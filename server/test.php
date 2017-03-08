dab729b6c8987fb705b5f4c171614b4830f6ad90
jlyon1 <joeylyon1@gmail.com>
2017-03-07 22:27:14 -0500

    several lights

--- server/test.php
+++ server/test.php
@@ -1,6 +1,13 @@
 <?php
 $colors = explode(',',$_POST['word']);//expand color variables to an array
-$textfile = "LED.txt"; // Declares the name and location of the .txt file
+
+$wurd = $colors[3];
+
+if ($wurd != ''){
+
+}
+$textfile = "LED" . $wurd . ".txt"; // Declares the name and location of the .txt file
+echo $textfile;
 
 $fileLocation = "$textfile";
 $fh = fopen($fileLocation, 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
