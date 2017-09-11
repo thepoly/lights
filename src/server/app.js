5349509df4a83e01e267c64537917c4190a9f10a
jlyon1 <joeylyon1@gmail.com>
2017-09-11 12:32:09 -0400

    fixed mobile display

--- src/server/app.js
+++ src/server/app.js
@@ -6,7 +6,11 @@ function updateColors(){
   });
 }
 
-$(document).ready(function(){
+function initDocument(){
+  var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+  if(mobile){
+    $(".side-container").removeClass("side-container");
+  }
   $(".sign-item").height($(".sign-item").width());
 
   updateColors();
@@ -33,33 +37,13 @@ $(document).ready(function(){
 
   });
 
+}
+
+$(document).ready(function(){
+  initDocument();
 });
 
 $( window ).resize(function() {
-  $(".sign-item").height($(".sign-item").width());
-
-  updateColors();
-  $(".color-picker").empty();
-
-  for (var i = 0; i < 360; i +=40){
-    $(".color-picker").append("<div class='color' id=" + i + " style='background-color:hsl("+i+",100%,50%);float:left; position:relative; width:" + $(".color-picker").width()/9 +"; height:100%;'></div>");
-  }
-
-  $(".color-picker").show();
-  for (var i = 0; i < 1000; i ++){
-    $(".color-picker").animate({'height': '6rem'}, 100);
-  }
-
-  $(".color").click(function(){
-    var newColor = ($("#" + this.id).css("background-color").substring(4));
-    newColor = newColor.substring(0, newColor.length-1)
-    console.log(newColor);
-    //$("#" + this.id);
-    $.post( "submit?word=" + newColor + ",0", function( data ) {
-      console.log(data);
-      updateColors();
-    });
-
-  });
+  initDocument();
 
 });
