02d38e4b936d2426cbdcc6244cedbb7434442e0c
jlyon1 <joeylyon1@gmail.com>
2017-10-19 11:10:33 -0400

    fixed some graphical glitches

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,22 @@
+function populatePicker(){
+  $(".cpicker").empty();
+
+  for (var i = 0; i < 360; i +=90){
+    $(".cpicker").append("<div class='color' id=" + i + " style='background-color:hsl("+i+",100%,50%);float:right; position:relative; width:" + ($(".cpicker").width())/4 +"; height:100%;'></div>");
+  }
+}
+
 function updatePageColor(){
   $.get( "LEDP.txt", function( data ) {
     data = data.split("\n");
-    $(".bg").css("background-color", "rgb(" + data[0] + "," +data[1]+ ","+data[2] +")");
+    $(".header").css("background-color", "rgb(" + data[0] + "," +data[1]+ ","+data[2] +")");
   });
 }
 
 var colorPicker = Vue.component('color-picker', {
   template: `
     <div>
-      <div v-on:click='updateColor' class="cpicker" style="padding-left:0;height:25%;width:100%;border-radius:5px; border-color:black; border-style: solid;"></div>
+      <div v-on:click='updateColor' class="cpicker" style="padding-left:0;height:25%;width:100%;border-radius:5px; border-color:black; border-style: solid;float:left;"></div>
     </div>`
     ,
   methods:{
@@ -29,9 +37,7 @@ var colorPicker = Vue.component('color-picker', {
     }
   },
   mounted(){
-    for (var i = 0; i < 360; i +=40){
-      $(".cpicker").append("<div class='color' id=" + i + " style='background-color:hsl("+i+",100%,50%);float:left; position:relative; width:" + $(".cpicker").width()/9 +"; height:100%;'></div>");
-    }
+    populatePicker();
     updatePageColor();
 
     setInterval(function(){
@@ -42,9 +48,8 @@ var colorPicker = Vue.component('color-picker', {
 })
 
 $( window ).resize(function() {
-  for (var i = 0; i < 360; i +=40){
-    $(".cpicker").append("<div class='color' id=" + i + " style='background-color:hsl("+i+",100%,50%);float:left; position:relative; width:" + $(".cpicker").width()/9 +"; height:100%;'></div>");
-  }
+  console.log("asdf");
+  populatePicker();
 });
 
 Vue.component('app-body', {
@@ -53,7 +58,6 @@ Vue.component('app-body', {
   <div>
     <div class="header"><img class="title-logo" src="images/logo_m.png"/></div>
     <div class ="project-title"><p class="main-header">PolyLights</p></div>
-    <div class = "bg" style="float:none; position:absolute; top:0;right:0;width:100%;height:100%;"></div>
     <div style="margin: 0 auto; width:75%;top:50px;position:relative;"><color-picker/></div>
     <div class="sub-text">Change the color of <a class="link" href="https://poly.rpi.edu">The Rensselaer Polytechnic's</a> Lights in the Rensselaer Student Union.
     </div>
